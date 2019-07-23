9de68457d819b535de4ed98e210217d316183c91
shuaimeng <980472035@qq.com>
2019-07-23 10:51:50 +0800

    update 删除公司信息

--- jms/activemq_se/src/main/java/com/meng/quere/QuereConsumerDemo.java
+++ jms/activemq_se/src/main/java/com/meng/quere/QuereConsumerDemo.java
@@ -1,21 +1,4 @@
-/*
- * Project Name: mqtt
- * File Name: QuereConsumerDemo.java
- * Class Name: QuereConsumerDemo
- *
- * Copyright 2018 Hengtian Software Inc
- *
- * Licensed under the Hengtiansoft
- *
- * http://www.hengtiansoft.com
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.meng.quere;
 
 import org.apache.activemq.ActiveMQConnection;
--- mqtt/src/main/java/SubMsg2.java
+++ mqtt/src/main/java/SubMsg2.java
@@ -1,46 +1,4 @@
-/*
- * Project Name: ids
- * File Name: SubMsg2.java
- * Class Name: SubMsg2
- *
- * Copyright 2018 Hengtian Software Inc
- *
- * Licensed under the Hengtiansoft
- *
- * http://www.hengtiansoft.com
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-/**
- * Description: TODO
- *
- * @author shuaimeng
- * @since 29.03.2019
- */
-/*
- * Project Name: ids
- * File Name: SubMsg.java
- * Class Name: SubMsg
- *
- * Copyright 2018 Hengtian Software Inc
- *
- * Licensed under the Hengtiansoft
- *
- * http://www.hengtiansoft.com
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
